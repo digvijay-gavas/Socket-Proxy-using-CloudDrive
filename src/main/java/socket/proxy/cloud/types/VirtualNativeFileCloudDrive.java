8f34d300bf5c565679d25e18a7086917aa4e5df8
49151 <49151@LAPTOP-GRNUJN87>
2019-07-28 19:30:24 +0200

    added VirtualNativeFileCloudDrive

--- src/main/java/socket/proxy/cloud/types/VirtualNativeFileCloudDrive.java
+++ src/main/java/socket/proxy/cloud/types/VirtualNativeFileCloudDrive.java
@@ -1,6 +1,10 @@
 package socket.proxy.cloud.types;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 
 
 
@@ -19,22 +23,28 @@ public class VirtualNativeFileCloudDrive implements CloudDrive{
 
 	@Override
 	public void uploadFile(String uploadFileName, byte[] inputBytes, int offset, int length) throws Exception {
-
+		FileOutputStream fileOutputStream=new FileOutputStream(new File(uploadFileName));
+		fileOutputStream.write(inputBytes,offset,length);
+		fileOutputStream.flush();
+		fileOutputStream.close();
 	}
 
 
 	@Override
 	public byte[] downloadFile(String downloadFileName) throws FileNotFoundException,Exception {
-		return  null;
+		FileInputStream fileInputStream=new FileInputStream(new File(downloadFileName));
+		byte[] bs=fileInputStream.readAllBytes();
+		fileInputStream.close();
+		return bs;
 	}
 
 	@Override
 	public void deleteFile(String deleteFileName) throws Exception {
-		
+		new File(deleteFileName).delete();
 	}
 
 	@Override
 	public boolean isFileExist(String deleteFileName) throws Exception {
-		return false;
+		return new File(deleteFileName).exists();
 	}
 }
